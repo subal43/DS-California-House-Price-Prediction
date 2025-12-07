22772c76e76ad75d85557b8d792f49e03dcdabcb
subal43 <subalkundu3@gmail.com>
2025-12-07 21:34:18 +0530

    fixed for render 1

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
-import os
-os.environ["OMP_NUM_THREADS"] = "1"
-os.environ["MKL_NUM_THREADS"] = "1"
-os.environ["KMP_INIT_AT_FORK"] = "FALSE"
-os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
-
-
 from flask import Flask, render_template, request, jsonify
 import joblib
 import pandas as pd
